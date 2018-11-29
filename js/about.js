179321ca61c64979e8c244310133cc4d4ed311a2
ZachZeng <ZachZeng@users.noreply.github.com>
2018-11-28 21:30:41 -0500

    s

---
+++ js/about.js
@@ -0,0 +1,12 @@
+var checkScrollBars = function(){
+    var b = $('body');
+    var normalw = 0;
+    var scrollw = 0;
+    if(b.prop('scrollHeight')>b.height()){
+        normalw = window.innerWidth;
+        scrollw = normalw - b.width();
+        $('.navigation').css({marginRight:'-'+scrollw+'px'});
+    }
+}
+
+checkScrollBars();
