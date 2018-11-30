144bb91d81cc4eb29ccfb0fa0420c59015484c58
ZachZeng <ZachZeng@users.noreply.github.com>
2018-11-29 19:33:01 -0500

    s

--- js/general.js
+++ js/general.js
@@ -1,7 +1,7 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("mySidebar").style.width = "30%";
-    document.getElementById("main").style.marginLeft = "30%";
+    document.getElementById("mySidebar").style.width = "300px";
+    document.getElementById("main").style.marginLeft = "300px";
     $('.openbtn').css("opacity","0");
 }
 
