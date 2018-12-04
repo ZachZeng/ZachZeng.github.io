6c082b4ed41c8cfe93ea0207a35dd7b37a3acef5
ZachZeng <ZachZeng@users.noreply.github.com>
2018-12-04 16:57:44 -0500

    s

--- js/general.js
+++ js/general.js
@@ -1,13 +1,14 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
     document.getElementById("mySidebar").style.width = "300px";
-    document.getElementById("main").style.marginLeft = "300px";
+    document.getElementById("main").style.paddingLeft = "300px";
     $('.openbtn').css("opacity","0");
+    $('html').css("width","100vm");
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("mySidebar").style.width = "0"; 
+    document.getElementById("main").style.paddingLeft = "0";
     $('.openbtn').css("opacity","0.9");
 }
